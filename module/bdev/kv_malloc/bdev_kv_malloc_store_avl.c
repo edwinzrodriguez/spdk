a39bcdd2dd55fdf009eb280d288378abbc3eaf7f
Jeff Semke <semke@netapp.com>
2021-03-13 00:41:03 -0500

    Fix key/val and len types.  Create list and implement insert.

--- module/bdev/kv_malloc/bdev_kv_malloc_store_avl.c
+++ module/bdev/kv_malloc/bdev_kv_malloc_store_avl.c
@@ -10,21 +10,33 @@
 #include "spdk/bdev_module.h"
 #include "bdev_kv_malloc_store.h"
 
+void
+kv_malloc_store_init(void)
+{
+	/* TODO */
+}
+
+void
+kv_malloc_store_destroy(void)
+{
+	/* TODO */
+}
+
 /* TODO still need to determine best way to manage values in/out without copy.  What is data lifecycle? */
 enum spdk_bdev_io_status
-kv_malloc_get(uint32_t *key, uint8_t key_size, void **value_out, uint32_t *value_size) {
+kv_malloc_get(uint8_t *key, uint32_t key_size, void **value_out, uint32_t *value_size) {
 	/* TODO */
 	return SPDK_BDEV_IO_STATUS_SUCCESS;
 }
 
 enum spdk_bdev_io_status
-kv_malloc_insert(uint32_t *key, uint8_t key_size, void *value_in, uint32_t value_size) {
+kv_malloc_insert(uint8_t *key, uint32_t key_size, void *value_in, uint32_t value_size) {
 	/* TODO */
 	return SPDK_BDEV_IO_STATUS_SUCCESS;
 }
 
 enum spdk_bdev_io_status
-kv_malloc_delete(uint32_t *key, uint8_t key_size) {
+kv_malloc_delete(uint8_t *key, uint32_t key_size) {
 	/* TODO */
 	return SPDK_BDEV_IO_STATUS_SUCCESS;
 }
